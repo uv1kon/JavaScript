d89b6fcf814b43983142d6a5a057b075170379e4
Victor <ddarho52@gmail.com>
2026-05-06 06:09:23 +0500

    Запусти у себя пжпжпж

--- script.js
+++ script.js
@@ -8,9 +8,25 @@
 //
 //prompt("Ну ты точно сосал же?", "да");
 var counter = "Ты гей!";
-function onClickButton() {
-    counter += "Ты гей!";
+var k = 2;
+function onClickButton(el) {
+    if (k %2 == 0) {el.style.backgroundColor = "black";
+        el.style.color = "white";
+        counter = "Ты гей!"
+        el.innerHTML = counter;}
 
-    console.log(counter);
+    if (k %2 ==1){el.style.backgroundColor = "white";
+        el.style.color = "black";
+        counter = "Ты чмо!"
+        el.innerHTML = counter;}
+    k++;
+
+}
+
+function onInput(el) {
+    if (el.value === "Ильхан" || el.value === "ильхан") {
+        console.log("Ассаламуалейкум", el.value);
+    }
+
 }
 

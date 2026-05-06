cafef34d498d093d12d082e256b41c77b4b531e0
Victor <ddarho52@gmail.com>
2026-05-06 05:40:20 +0500

    Работа с кнопкой

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-alert("Ты питух");
-var data = confirm("Сосал?");
-if (data) {
-    alert("Хорош");
-} else {
-    confirm("Точно?");
+//alert("Ты питух");
+//var data = confirm("Сосал?");
+//if (data) {
+//    alert("Хорош");
+//} else {
+//    confirm("Точно?");
+//}
+//
+//prompt("Ну ты точно сосал же?", "да");
+var counter = "Ты гей!";
+function onClickButton() {
+    counter += "Ты гей!";
+
+    console.log(counter);
 }
 
-prompt("Ну ты точно сосал же?", "да");

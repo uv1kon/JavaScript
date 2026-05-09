e655f47752e6b7fc2590022e319d5e682a1d3fa6
Victor <ddarho52@gmail.com>
2026-05-10 02:07:46 +0500

    Переход на реакт щас будет

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
-//alert("Ты питух");
-//var data = confirm("Сосал?");
-//if (data) {
-//    alert("Хорош");
-//} else {
-//    confirm("Точно?");
-//}
-//
-//prompt("Ну ты точно сосал же?", "да");
-var counter = "Ты гей!";
-var k = 2;
-function onClickButton(el) {
-    if (k %2 == 0) {el.style.backgroundColor = "black";
-        el.style.color = "white";
-        counter = "Ты гей!"
-        el.innerHTML = counter;}
+function checkForm(el){
+    var name = el.name.value;
+    var pass = el.pass.value;
+    var repass = el.repass.value;
+    var state = el.state.value;
 
-    if (k %2 ==1){el.style.backgroundColor = "white";
-        el.style.color = "black";
-        counter = "Ты чмо!"
-        el.innerHTML = counter;}
-    k++;
+    console.log(name, pass, state);
 
+    return false;
 }
-
-function onInput(el) {
-    if (el.value === "Ильхан" || el.value === "ильхан") {
-        console.log("Ассаламуалейкум", el.value);
-    }
-
-}
-
